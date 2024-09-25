a32ab147f686c9f48de839f9558dbb879cb3710e
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-09-25 20:57:42 +0100

    otimizei o erro do zod

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,9 +1,14 @@
 import { AppError } from "@/util/AppError";
 import { NextFunction, Response, Request } from "express";
+import { ZodError } from "zod";
 
 export function errorHandling(error: any, request: Request, response: Response, _:NextFunction) { 
   if (error instanceof AppError ) {
     return response.status(error.statusCode).json({message: error.message})
+  }
+
+  if (error instanceof ZodError ) {
+      return response.status(400).json({ message:"validation error", issues: error.format() })
   }
 
   return response.status(500).json({ message: error.message })
