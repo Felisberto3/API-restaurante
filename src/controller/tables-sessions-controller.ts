cb06245b34d899468f799ce321787c029ed34a49
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-09-27 16:50:30 +0100

    create sessions type

--- src/controller/tables-sessions-controller.ts
+++ src/controller/tables-sessions-controller.ts
@@ -1,5 +1,6 @@
 import { knex } from "@/database/knex"
 import { Request, NextFunction, Response  } from "express"
+import { z } from "zod"
 class TableSessionsController {
     async index(request: Request, response:Response, next:NextFunction) {
       try {
@@ -13,7 +14,17 @@ class TableSessionsController {
 
     async create(request: Request, response:Response, next:NextFunction) {
       try {
-          const table_session = await knex("table_sessions").select()
+        const bodySchema = z.object({
+          table_id: z.number({
+            required_error:"não informaste o id",
+            invalid_type_error:"o id deve ser um número"
+          })
+        })
+
+        const { table_id } = bodySchema.parse(request.body)
+
+          const table_session = await knex("tables_sessions").select()
+
           return response.json(table_session)
       } catch (error) {
         next(error)
